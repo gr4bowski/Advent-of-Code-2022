1a73b8d7065a693aa914a0de8694b8299ee24278
gr4bowski <112784743+gr4bowski@users.noreply.github.com>
2022-12-05 17:35:15 +0100

    Update day5part1.java

--- day5part1.java
+++ day5part1.java
@@ -1,30 +1,69 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class day5part1 {
+public class d5p1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        LinkedList[] stacks = new LinkedList[8];
-        LinkedList stack = new LinkedList<>();
-
+        String nextLine;
+        LinkedList[] stacks = new LinkedList[9];
+        LinkedList<Character> stack1 = new LinkedList<>();
+        stacks[0] = stack1;
+        LinkedList<Character> stack2 = new LinkedList<>();
+        stacks[1] = stack2;
+        LinkedList<Character> stack3 = new LinkedList<>();
+        stacks[2] = stack3;
+        LinkedList<Character> stack4 = new LinkedList<>();
+        stacks[3] = stack4;
+        LinkedList<Character> stack5 = new LinkedList<>();
+        stacks[4] = stack5;
+        LinkedList<Character> stack6 = new LinkedList<>();
+        stacks[5] = stack6;
+        LinkedList<Character> stack7 = new LinkedList<>();
+        stacks[6] = stack7;
+        LinkedList<Character> stack8 = new LinkedList<>();
+        stacks[7] = stack8;
+        LinkedList<Character> stack9 = new LinkedList<>();
+        stacks[8] = stack8;
         for (int level = 0; level < 8; level++){
-            String nextLine = scanner.nextLine();
+            nextLine = scanner.nextLine();
             for (int n = 0; n < 8; n++) {
                 int barrelIndex = (4 * n) + 1;
-                if (level == 0) {
-                    stacks[n] = stack;
-                }
                 if (!(nextLine.charAt(barrelIndex) == ' ')) {
                     Character barrel = nextLine.charAt(barrelIndex);
-                    stack = stacks[n];
-                    stack.addLast(barrel);
-                    stacks[n] = stack;
+                    stacks[n].add(barrel);
                 }
             }
+        }
+        System.out.println("Orders:");
+        do {
+            nextLine = scanner.nextLine();
+            if (!nextLine.isEmpty()) {
+                nextLine = nextLine.replaceAll("[^0-9]", " ");
+                nextLine = nextLine.replaceAll(" +", " ");
+                String[] nextLineS = nextLine.split(" ");
+                int x = Integer.parseInt(nextLineS[1]);
+                int y = Integer.parseInt(nextLineS[2])-1;
+                int z = Integer.parseInt(nextLineS[3])-1;
+                for (int i = 0; i < x; i++) {
+                    if (!stacks[y].isEmpty()) {
+                        Object toBeMoved = stacks[y].getFirst();
+                        stacks[z].addFirst(toBeMoved);
+                        stacks[y].removeFirst();
+                    }
+                }
 
+            } else {
+                break;
+            }
+        } while (true);
+        for (int top = 0; top < 9; top++) {
+            if (!stacks[top].isEmpty()) {
+                Character atTop = (Character) stacks[top].getFirst();
+                System.out.print(atTop);
+            } else {
+                System.out.print(" ");
+            }
         }
-        for (int x = 0; x < 8; x++) {
-            System.out.println(stacks[x]);
-        }
+        System.out.println();
     }
 }
