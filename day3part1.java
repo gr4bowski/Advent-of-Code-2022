5920de891e72b7793224edbd6d8b730cecaf9f6f
Julek <xjuliuszpl@icloud.com>
2022-12-05 12:43:40 +0100

    Day 3

---
+++ day3part1.java
@@ -0,0 +1,9 @@
+public class day3part1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            String nextLine = scanner.nextLine;
+            System.out.println(nextLine);
+        } while (!nextLine.isEmpty)
+    }
+}
